e133d24d1bf0d160bab0adfab5d40293f6fcb803
Mark Garcia <111922999+MarkRyanGarcia@users.noreply.github.com>
2025-12-14 03:37:36 -0800

    fix spacing of home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { FiLinkedin, FiGithub } from "react-icons/fi";
+import { FiGithub } from "react-icons/fi";
 import { CiLinkedin } from "react-icons/ci";
 import { NavLink } from "react-router-dom";
 
@@ -8,18 +8,18 @@ export default function Home() {
     return (
         <div className="w-full py-15">
             <div className="flex">
-                <div className="flex flex-col w-full space-y-5">
+                <div className="flex flex-col w-[55%] space-y-5">
                     <h1 className="text-primary text-3xl font-bold">
                         Hey, I'm <span className="text-tertiary">Mark Garcia</span>
                     </h1>
-                    <p className="text-secondary">
+                    <p className="text-secondary text-base">
                         I’m a part-time SWE @ <a className={linkStyle} href="https://glenair.com/" target="_blank">Glenair, Inc.</a> I’m currently a
                         4th year student at CSU Fullerton, where I am the
                         Fullerton Chapter President of the <a className={linkStyle} href="https://acmcsuf.com/" target="_blank">Association for
-                            Computing Machinery</a> (ACM).
+                            Computing Machinery (ACM)</a>.
                     </p>
                 </div>
-                <div className="flex w-full justify-center">
+                <div className="flex w-[45%] justify-center">
                     <img src="/mark/mark.png" className="w-50 rounded-full border-4 border-secondary hover:border-tertiary transition-colors duration-200 ease-in-out" />
                 </div>
             </div>
