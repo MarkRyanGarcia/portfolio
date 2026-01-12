234ff899823df7b490ba115ad74f7ad11fc1b1ff
Mark Garcia <111922999+MarkRyanGarcia@users.noreply.github.com>
2026-01-11 21:07:18 -0800

    fix about spacing

--- src/components/about/AboutImage.tsx
+++ src/components/about/AboutImage.tsx
@@ -1,9 +1,9 @@
 export default function AboutImage() {
     return (
-        <div className="flex justify-center w-full max-w-75 md:max-w-70">
+        <div className="flex justify-center w-full max-w-75 md:max-w-70 mx-auto">
             <img
                 src="/mark/cruise_pic.JPG"
-                className="w-full max-w-xs md:max-w-sm rounded-md bg-secondary object-cover"
+                className="w-full rounded-md bg-secondary object-cover"
                 alt="Mark Garcia"
                 loading="lazy"
             />
--- src/components/about/Intro.tsx
+++ src/components/about/Intro.tsx
@@ -4,7 +4,7 @@ import AboutLinks from "./AboutLinks";
 
 export default function Intro() {
     return (
-        <div className="flex flex-col w-full gap-6 p-4 md:justify-center md:flex-row md:items-center bg-base-2 border-secondary border rounded-lg text-primary">
+        <div className="flex flex-col w-full gap-6 p-4 md:flex-row md:items-center bg-base-2 border-secondary border rounded-lg text-primary">
             <AboutImage />
 
             <div className="flex flex-col w-full gap-4 md:w-[60%] md:px-6">
@@ -12,58 +12,5 @@ export default function Intro() {
                 <AboutLinks />
             </div>
         </div>
-    )
-    {/* <div className="flex flex-col gap-5 w-full md:items-center bg-base-2 border-secondary border rounded-lg text-primary">
-            <div
-                className={`flex flex-col w-full gap-3 ${reverse ? "md:flex-row-reverse" : "md:flex-row"
-                    } items-center justify-around text-center sm:text-left p-5`}
-            >
-                <div className="flex flex-col gap-2 items-center text-center">
-                    <h3 className="text-xl text-tertiary font-bold">
-                        {project.title}
-                    </h3>
-
-                    <p className="max-w-xl">
-                        {project.description}
-                    </p>
-
-                    {project.links && (
-                        <div className="flex gap-2 justify-center">
-                            {project.links.Deployment && (
-                                <a
-                                    href={project.links.Deployment}
-                                    target="_blank"
-                                    rel="noopener noreferrer"
-                                    className="flex items-center hover:text-base hover:bg-tertiary transition-all duration-300 ease-in-out border py-0.5 px-1.5 rounded-md hover:-translate-y-0.5"
-                                >
-                                    <MdLanguage className="mr-1.5" />
-                                    Visit Site
-                                </a>
-                            )}
-
-                            {project.links.Github && (
-                                <a
-                                    href={project.links.Github}
-                                    target="_blank"
-                                    rel="noopener noreferrer"
-                                    className="flex items-center hover:text-base hover:bg-tertiary transition-all duration-300 ease-in-out border py-0.5 px-1.5 rounded-md hover:-translate-y-0.5"
-                                >
-                                    <FiGithub className="mr-1.5" />
-                                    GitHub
-                                </a>
-                            )}
-                        </div>
-                    )}
-                </div>
-
-                <img
-                    className="w-75 rounded-sm"
-                    src={project.image_path}
-                    alt={`${project.title} image`}
-                    loading="lazy"
-                />
-            </div>
-        </div> */}
-
-    
+    )    
 }
