18a3aca686996301650b6629c6cf63008e59e03c
Mark Garcia <111922999+MarkRyanGarcia@users.noreply.github.com>
2026-01-11 19:57:23 -0800

    make resume redirect more reliable

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -1,10 +1,13 @@
+import { useEffect } from "react";
 import { linkStyle } from "../lib/styles";
 
 
 export default function Resume() {
     const resumeUrl = "https://docs.google.com/viewerng/viewer?url=https://raw.githubusercontent.com/MarkRyanGarcia/Resume/main/Mark_Garcia_Resume.pdf";
 
-    window.location.href = resumeUrl;
+    useEffect(() => {
+        window.location.href = resumeUrl;
+    }, []);
 
     return (
         <div className="flex flex-col items-center justify-center text-primary text-center space-y-4">
