67b85f67c9696ab6d394bab8fadf10619c39138e
Mark Garcia <111922999+MarkRyanGarcia@users.noreply.github.com>
2026-01-11 21:02:50 -0800

    improve about me page

--- src/components/about/AboutImage.tsx
+++ src/components/about/AboutImage.tsx
@@ -1,6 +1,6 @@
 export default function AboutImage() {
     return (
-        <div className="flex justify-center w-full md:w-[40%]">
+        <div className="flex justify-center w-full max-w-75 md:max-w-70">
             <img
                 src="/mark/cruise_pic.JPG"
                 className="w-full max-w-xs md:max-w-sm rounded-md bg-secondary object-cover"
