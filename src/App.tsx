792d1c8a8ad5fa0f841f50f204e977b8614b8ff4
Mark Garcia <111922999+MarkRyanGarcia@users.noreply.github.com>
2026-01-11 16:40:46 -0800

    fix navbar blur bug

--- src/App.tsx
+++ src/App.tsx
@@ -11,27 +11,26 @@ export default function App() {
     return (
         <TooltipProvider>
             <Router>
-                {/* 1. This wrapper ensures the content always fills at least the full screen */}
-                <div className="flex flex-col min-h-screen max-w-4xl mx-auto">
-
+                <div className='flex flex-col min-h-screen'>
                     <Navbar />
+                    <div className="max-w-4xl mx-auto">
+                        <main className='grow max-w-4xl mx-auto w-full px-5 md:px-0 pt-5 md:pt-15'>
+                            <Routes>
+                                <Route path="/" element={<Home />} />
+                                <Route path="/about" element={<About />} />
+                                <Route path="/projects" element={<Projects />} />
+                                <Route path="/contact" element={<Contact />} />
+                                <Route path="resume" Component={() => {
+                                    window.location.href = 'https://docs.google.com/viewerng/viewer?url=https://raw.githubusercontent.com/MarkRyanGarcia/Resume/main/Mark_Garcia_Resume.pdf';
+                                    return null;
+                                }} />
+                            </Routes>
+                        </main>
 
-                    {/* 2. This container grows to fill all space between Navbar and Footer */}
-                    <main className='grow max-w-4xl mx-auto w-full px-5 md:px-0 pt-5 md:pt-15'>
-                        <Routes>
-                            <Route path="/" element={<Home />} />
-                            <Route path="/about" element={<About />} />
-                            <Route path="/projects" element={<Projects />} />
-                            <Route path="/contact" element={<Contact />} />
-                            <Route path="resume" Component={() => {
-                                window.location.href = 'https://docs.google.com/viewerng/viewer?url=https://raw.githubusercontent.com/MarkRyanGarcia/Resume/main/Mark_Garcia_Resume.pdf';
-                                return null;
-                            }} />
-                        </Routes>
-                    </main>
-
-                    <Footer />
+                        <Footer />
+                    </div>
                 </div>
+
             </Router>
         </TooltipProvider>
     );
