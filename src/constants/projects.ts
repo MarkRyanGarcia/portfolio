bfce6e3cb4475f1fb3249cea2e3de0ce941bf18e
Mark Garcia <contact@markgarcia.dev>
2026-08-07 20:51:33 -0700

    replace ffa demo gif

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -41,7 +41,7 @@ export const projects: Project[] = [
         title: "fastfastapi",
         description:
             "A modular fastapi app generation tool. No bloated/overkill boilerplate, only include the capabilities you need. Inspired by create-vite. Support for PostgreSQL with SQLAlchemy, SQLModel, or FastCRUD; MongoDB via PyMongo; Redis caching; and OIDC authentication",
-        image_path: "/ProjectImages/fapi-init-demo.gif",
+        image_path: "/ProjectImages/ffa_demo.gif",
         featured: false,
         tags: ["Go", "CLI"],
         links: {
